6c3162b7e14eb18d66b46a30a697dae53da39620
SongYuan1995 <838523504@qq.com>
2019-09-29 11:03:05 +0800

    BinaryTree.java

---
+++ src/Tree/BinaryTree.java
@@ -0,0 +1,14 @@
+package Tree;
+
+public class BinaryTree {
+    //根节点
+    TreeNode root;
+
+    public void setRoot(TreeNode root) {
+        this.root = root;
+    }
+
+    public TreeNode getRoot() {
+        return root;
+    }
+}
